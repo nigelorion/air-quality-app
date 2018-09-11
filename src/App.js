6c0e25a6850a8ddcd75d124134e951b3b49313f6
nigelorion <nigelorion@gmail.com>
2018-09-11 10:04:23 -0700

    add get location function, responsive layout changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-// import Moment from 'react-moment';
 import moment from 'moment-timezone';
 
 class App extends Component {
-  
   constructor(props) {
     super(props);
-
     this.aqiKey = `${process.env.REACT_APP_aqi_key}`;
     this.weatherKey = `${process.env.REACT_APP_weather_key}`;
-
     this.state = {
       aqiData: {},
       weatherData: {},
+      geoData: {},
       loaded: false,
-      weatherLoaded: false
+      weatherLoaded: false,
+      geoLoaded: false
     };
   }
+
   componentDidMount() {
     this.nearestCity()
     this.weatherForcast()
-    console.log(this.weatherKey)
+    this.getGeo();
   }
+
   nearestCity() {
-  
     this.setState({loaded: false});
     fetch("https://api.airvisual.com/v2/nearest_city?key=" + this.aqiKey)
     .then(response => response.json())
@@ -37,10 +36,9 @@ class App extends Component {
         city: "Portland"
       });
     })
-
   }
-  weatherForcast() {
 
+  weatherForcast() {
     this.setState({weatherLoaded: false});
     fetch("https://api.weatherbit.io/v2.0/current?ip=auto&key=" + this.weatherKey)
     .then(response => response.json())
@@ -51,57 +49,72 @@ class App extends Component {
       })
       console.log(this.state.weatherData[0])
     })
-
   }
 
   timeConverter(time) {
-  
     let timezone = this.state.weatherData[0].timezone;
-
-    let result = moment.utc(time, 'HH:mm').tz(timezone).format('h:mm a');
-
+    let result = moment.utc(time, 'HH:mm').tz(timezone).format('h:mm a')
     return result
-  
   }
 
-
   convertToF(celsius) {
-
     let fahrenheit = (celsius * (9/5)) + 32;
     fahrenheit = Math.round(fahrenheit);
     return fahrenheit;
-
   }
+
+  getGeo() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(response => {
+        this.setState({
+          geoData: response,
+          geoLoaded: true
+        })
+        console.log(this.state.geoData)
+      })
+    } else {
+      console.log("geolocation not available")
+    }
+  }
+
   render() {
     return (
       <div className="App"> 
-        <div className="header">
-          <h2>{this.state.aqiData.city}, {this.state.aqiData.state}</h2>
-          <button onClick={this.componentDidMount.bind(this)}>update</button>
-        </div>
-        <div className="boxContainer">
-          <div className="aqiBox">
-            {!this.state.loaded &&
-              <img src={"/lg.rainy-preloader.gif"}alt="loading"></img>
-        
-            }
-            {this.state.loaded &&
+        <div className="container">
+          <div className="header">
+            <p>{this.state.aqiData.city}, {this.state.aqiData.state}</p>
+            {this.state.geoLoaded && 
               <div>
-                <p  className="temp">{this.state.aqiData.current.pollution.aqius}<span className="degrees">  air quality</span></p>
+                <p>{this.state.geoData.coords.longitude}</p>
+                <p>{this.state.geoData.coords.latitude}</p>
               </div>
             }
+            <button onClick={this.componentDidMount.bind(this)}>update</button>
           </div>
-          <div className="weatherBox">
-            {!this.state.weatherLoaded &&
-              <img src={"/lg.rainy-preloader.gif"} alt="loading"></img>
-            }
-            {this.state.weatherLoaded &&
-              <div>
-                <p className="temp">{this.convertToF(this.state.weatherData[0].app_temp)}<span className="degrees">  degrees</span></p>
-                <p>{this.timeConverter(this.state.weatherData[0].sunrise)} sunrise</p>
-                <p>{this.timeConverter(this.state.weatherData[0].sunset)} sunset</p>
-              </div>
-            }
+          <div className="dataBox">
+            <div className="aqiBox">
+              {!this.state.loaded &&
+                <img src={"/lg.rainy-preloader.gif"}alt="loading"></img>
+          
+              }
+              {this.state.loaded &&
+                <div>
+                  <p  className="temp">{this.state.aqiData.current.pollution.aqius}<span className="degrees">  air quality</span></p>
+                </div>
+              }
+            </div>
+            <div className="weatherBox">
+              {!this.state.weatherLoaded &&
+                <img src={"/lg.rainy-preloader.gif"} alt="loading"></img>
+              }
+              {this.state.weatherLoaded &&
+                <div>
+                  <p className="temp">{this.convertToF(this.state.weatherData[0].app_temp)}<span className="degrees">  degrees</span></p>
+                  <p>{this.timeConverter(this.state.weatherData[0].sunrise)} sunrise</p>
+                  <p>{this.timeConverter(this.state.weatherData[0].sunset)} sunset</p>
+                </div>
+              }
+            </div>
           </div>
         </div>
       </div>
